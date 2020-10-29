382cb9bb0c595994f33a45e0375190dec1f4f962
usworder@gmail.com <G0g0pr4y>
2020-10-29 07:56:44 +0700

    add fragment for result

---
+++ app/src/main/java/com/inbedroom/couriertracking/view/OngkirDetailFragment.kt
@@ -0,0 +1,30 @@
+package com.inbedroom.couriertracking.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.inbedroom.couriertracking.R
+
+class OngkirDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_ongkir_detail, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            OngkirDetailFragment().apply {
+
+            }
+    }
+}
