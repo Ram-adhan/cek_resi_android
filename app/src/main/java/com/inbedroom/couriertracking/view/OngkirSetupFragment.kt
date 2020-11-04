dfc240d5738ec7b6e0700a040d9160d5999f09aa
usworder@gmail.com <G0g0pr4y>
2020-11-04 08:28:28 +0700

    change chip style

--- app/src/main/java/com/inbedroom/couriertracking/view/OngkirSetupFragment.kt
+++ app/src/main/java/com/inbedroom/couriertracking/view/OngkirSetupFragment.kt
@@ -1,11 +1,13 @@
 package com.inbedroom.couriertracking.view
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.chip.Chip
@@ -19,6 +21,7 @@ import com.inbedroom.couriertracking.utils.Message
 import com.inbedroom.couriertracking.viewmodel.OngkirViewModel
 import kotlinx.android.synthetic.main.activity_cek_ongkir.*
 import kotlinx.android.synthetic.main.fragment_ongkir_setup.*
+import kotlinx.android.synthetic.main.info_text_view.view.*
 
 class OngkirSetupFragment : Fragment() {
 
@@ -47,6 +50,11 @@ class OngkirSetupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val colorState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            context?.getColorStateList(R.color.chip_text_state)
+        } else {
+            AppCompatResources.getColorStateList(requireContext(), R.color.chip_text_state)
+        }
         couriers.forEach {
             val chip = Chip(requireContext())
             val chipDrawable = ChipDrawable.createFromAttributes(
@@ -56,6 +64,7 @@ class OngkirSetupFragment : Fragment() {
                 R.style.CustomChipStyle
             )
             chip.setChipDrawable(chipDrawable)
+            chip.setTextColor(colorState)
             chip.text = it
             chip.isCheckable = true
             chipGroupCourier.addView(chip)
