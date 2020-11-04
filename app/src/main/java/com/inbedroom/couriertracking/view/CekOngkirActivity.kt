46ba2315526f1bf1519761798dc04a73165aa33d
usworder@gmail.com <G0g0pr4y>
2020-11-04 07:13:44 +0700

    change activity to fragment

--- app/src/main/java/com/inbedroom/couriertracking/view/CekOngkirActivity.kt
+++ app/src/main/java/com/inbedroom/couriertracking/view/CekOngkirActivity.kt
@@ -36,10 +36,7 @@ class CekOngkirActivity : BaseActivity() {
     lateinit var viewModelFactory: ViewModelFactory
 
     private lateinit var viewModel: OngkirViewModel
-    private val couriers = listOf("JNE", "Pos", "TIKI")
-    private val citiesName: MutableMap<String, String> = mutableMapOf()
 
-    private var requestCount = 0
 
     override fun layoutId(): Int = R.layout.activity_cek_ongkir
 
@@ -51,10 +48,6 @@ class CekOngkirActivity : BaseActivity() {
             viewModelFactory
         ).get(OngkirViewModel::class.java)
 
-        viewModel.isLoadingData.observe(this, loadingData)
-        viewModel.cityList.observe(this, cityList)
-        viewModel.failed.observe(this, failed)
-        viewModel.ongkirData.observe(this, ongkirData)
         viewModel.onRequest.observe(this, loadingRequest)
     }
 
@@ -62,81 +55,12 @@ class CekOngkirActivity : BaseActivity() {
         supportActionBar?.title = getString(R.string.tariff_check)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        couriers.forEach {
-            val chip = Chip(this)
-            val chipDrawable = ChipDrawable.createFromAttributes(
-                this,
-                null,
-                0,
-                R.style.Widget_MaterialComponents_Chip_Choice
-            )
-            chip.setChipDrawable(chipDrawable)
-            chip.text = it
-            chip.isCheckable = true
-
-            chipGroupCourier.addView(chip)
-        }
+        supportFragmentManager.beginTransaction()
+            .add(R.id.cekOngkirMainFragmentRoot, OngkirSetupFragment(), "ongkirSetup")
+            .commit()
     }
 
     override fun onAction() {
-
-        cekOngkirButtonCalculate.setOnClickListener {
-            var canContinue = true
-            val origin = cekOngkirEtOrigin.text.toString()
-            val destination = cekOngkirEtDestination.text.toString()
-            val weight = try {
-                cekOngkirEtWeight.text.toString().toInt()
-            } catch (e: NumberFormatException) {
-                0
-            }
-
-            if (origin.isEmpty()) {
-                cekOngkirEtOrigin.error = "Empty origin"
-                canContinue = false
-            } else if (!citiesName.containsKey(origin)) {
-                cekOngkirEtOrigin.error = "Unknown city, please type as suggested"
-                canContinue = false
-            }
-
-            if (destination.isEmpty()) {
-                cekOngkirEtDestination.error = "Empty destination"
-                canContinue = false
-            } else if (!citiesName.containsKey(destination)) {
-                cekOngkirEtOrigin.error = "Unknown city, please type as suggested"
-                canContinue = false
-            }
-
-            if (weight == 0) {
-                cekOngkirEtWeight.error = "Empty Weight"
-                canContinue = false
-            }
-
-            if (canContinue) {
-                val courierList = mutableListOf<String>()
-                chipGroupCourier.checkedChipIds.forEach {
-                    courierList.add(couriers[it - 1])
-                    requestCount++
-                }
-
-                val originString = citiesName[origin] ?: "-1"
-                val destinationString = citiesName[destination] ?: "-1"
-                viewModel.checkTariff(originString, destinationString, weight, courierList)
-            }
-        }
-    }
-
-    private val cityList = Observer<List<CityEntity>> { data ->
-        data.forEach {
-            val prefix = if (it.type.equals("kabupaten", true)) it.type + " " else ""
-            citiesName[prefix + it.cityName] = it.cityId
-        }
-        val adapter: ArrayAdapter<String> = ArrayAdapter(
-            this,
-            android.R.layout.simple_list_item_1,
-            citiesName.keys.toTypedArray()
-        )
-        cekOngkirEtOrigin.setAdapter(adapter)
-        cekOngkirEtDestination.setAdapter(adapter)
     }
 
     private val loadingRequest = Observer<Boolean> {
@@ -145,35 +69,22 @@ class CekOngkirActivity : BaseActivity() {
         } else {
             cekOngkirLoading.invisible()
             supportFragmentManager.beginTransaction()
-                .add(R.id.cekOngkirRoot, OngkirDetailFragment.forOngkir(), "ongkirList")
+                .addToBackStack("ongkirList")
+                .add(R.id.cekOngkirMainFragmentRoot, OngkirDetailFragment.forOngkir(), "ongkirList")
                 .commit()
         }
     }
 
-    private val ongkirData = Observer<List<OngkirResult>> {
-    }
-
-    private val loadingData = Observer<Boolean> {
-        if (it) {
-            loadingOrigin.visible()
-            loadingDestination.visible()
-        } else {
-            loadingOrigin.invisible()
-            loadingDestination.invisible()
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val mainFragment = supportFragmentManager.findFragmentByTag("ongkirList")
+        if (mainFragment != null && mainFragment.isVisible){
+            supportFragmentManager.popBackStack()
         }
     }
 
-    private val failed = Observer<String> {
-        Message.toast(this, it)
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        finish()
-    }
-
     override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
+        finish()
         return super.onSupportNavigateUp()
     }
 }
