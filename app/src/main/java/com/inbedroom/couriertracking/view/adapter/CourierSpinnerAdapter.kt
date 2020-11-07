db28d5c28d5372068161b2f9a386e619e06a99d9
usworder@gmail.com <G0g0pr4y>
2020-11-07 07:22:01 +0700

    import optimize

--- app/src/main/java/com/inbedroom/couriertracking/view/adapter/CourierSpinnerAdapter.kt
+++ app/src/main/java/com/inbedroom/couriertracking/view/adapter/CourierSpinnerAdapter.kt
@@ -4,10 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ArrayAdapter
 import com.bumptech.glide.Glide
 import com.inbedroom.couriertracking.R
-import com.inbedroom.couriertracking.data.entity.Courier
 import com.inbedroom.couriertracking.data.entity.SpinnerCourier
 import kotlinx.android.synthetic.main.item_courier.view.*
 
