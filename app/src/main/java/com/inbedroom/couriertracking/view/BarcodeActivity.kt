f8ce58f3eafab661a44ef78cae7818800168eec2
usworder@gmail.com <G0g0pr4y>
2021-01-10 12:00:07 +0700

    init cameraX and ml-kit

---
+++ app/src/main/java/com/inbedroom/couriertracking/view/BarcodeActivity.kt
@@ -0,0 +1,12 @@
+package com.inbedroom.couriertracking.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inbedroom.couriertracking.R
+
+class BarcodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barcode)
+    }
+}
